745704e793366afcf85f162f7427e07cdd774e49
JacobKnittle <jacobdknittle@gmail.com>
2026-09-03 14:00:38 -0700

    Add eventlisterner background color black when mouse enters

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'black'
+        })
         container.appendChild(square);
+        
     }
 }
+
