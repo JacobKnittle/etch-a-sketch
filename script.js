b8aa84f1c1c74dc144e5f632afae76f964461ed6
JacobKnittle <jacobdknittle@gmail.com>
2026-09-03 11:01:27 -0700

    generate grid of squares to screen

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container");
+
+// create 16 by 16 grid of square divs
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+}
